6856e785a755c61ad0da053e611f4f6e41db2d87
Azra ikram <asra010110@gmail.com>
2025-02-03 11:33:38 +0530

    QA:commended_azra

--- Cinnamore_Backend/src/index.ts
+++ Cinnamore_Backend/src/index.ts
@@ -8,6 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const uploadsDir = path.join(process.cwd(), 'uploads');
+// QA Note: Test the creation of the 'uploads' directory
+//Verify that the directory is created only if it does not exist, and check if it's created successfully.
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
   console.log('Uploads directory created!');
@@ -30,8 +32,12 @@ app.use(cinnamonRoutes);
 app.use(express.json());
 app.use("/api/price", priceRoutes);
 
-
+//QA Note: Verify the routing logic.
+//Ensure that '/api/price' routes work as expected by sending requests and confirming responses.
+//Verify that cinnamon routes are correctly routed and tested.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    // QA Note: Test server startup
+    //Ensure that the server starts without errors. Verify that the server is listening on the correct port.
 });
